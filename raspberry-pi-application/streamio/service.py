1e75aac17b7fe70ac75d34edcb2e57e01de34b2b
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-10-29 11:42:04 +0530

    init streamio service

---
+++ raspberry-pi-application/streamio/service.py
@@ -0,0 +1,15 @@
+import cv2
+class StreamIOService:
+    def __init__(self, camera_index=0):
+        self.cap = cv2.VideoCapture(camera_index)
+        if not self.cap.isOpened():
+            raise RuntimeError("Cannot open webcam")
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            raise RuntimeError("Failed to read frame from webcam")
+        return frame
+    
+    def release(self):
+        self.cap.release()
