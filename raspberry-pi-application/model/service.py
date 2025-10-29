383f461c6329943a9b2ed7dd872239f568d00814
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-10-29 11:47:39 +0530

    update model service

--- raspberry-pi-application/model/service.py
+++ raspberry-pi-application/model/service.py
@@ -3,18 +3,16 @@ import numpy
 import onnxruntime
 
 class ModelService:
-    def __init__(self, repo_id:str, file_name:str):
-        self.repo_id = repo_id
-        self.file_name = file_name
-        self.model_path = self._download_model()
+    def __init__(self, repo_id:str, file_name:str, revision:str):
+        model_path = self._download_model(repo_id, file_name, revision)
         self.session = onnxruntime.InferenceSession(
             self.model_path,
             providers=['CPUExecutionProvider']
         )
         
         
-    def _download_model(self) -> str:
-        model_path = hf_hub_download(repo_id=self.repo_id, filename=self.file_name)
+    def _download_model(self, repo_id:str, file_name:str, revision:str) -> str:
+        model_path = hf_hub_download(repo_id=repo_id, filename=file_name, revision=revision)
         return model_path
 
     def infer(self, frame:numpy.ndarray) -> numpy.ndarray:
